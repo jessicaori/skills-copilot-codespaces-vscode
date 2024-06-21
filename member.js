c6f3f28ed8aca1215b3e89ab71aa051b0e090fdf
Jessica Orihuela Rojas <66490864+jessicaori@users.noreply.github.com>
2024-06-21 03:34:38 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,29 @@
+function skillsmember() {
+    var skills = [
+        {
+            name: "HTML",
+            value: 90
+        },
+        {
+            name: "CSS",
+            value: 80
+        },
+        {
+            name: "JavaScript",
+            value: 70
+        },
+        {
+            name: "React",
+            value: 60
+        },
+        {
+            name: "NodeJS",
+            value: 50
+        },
+        {
+            name: "Python",
+            value: 40
+        }
+    ];
+    return skills;
+}
